eca7f05a8130fd7279e9f5ed94346a45ac8ff9f5
Alkamva <ngantwenistefano@gmail.com>
2020-02-18 16:38:02 +0200

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    public static void main(String[] args){
+
+        int x = 5;
+        int y = 10;
+        int sum = x + y;
+
+        System.out.println("The sum of two numbers is:" + sum);
+
+    }
+}
